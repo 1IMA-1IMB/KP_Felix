05c891c7149e7e0780d805d32362a16220ab4132
Glafyr <felixborrm@gmail.com>
2023-12-11 08:28:43 +0100

    sda
    dsw

--- newsblog/server/routes/main.js
+++ newsblog/server/routes/main.js
@@ -5,48 +5,76 @@ const Users = require('../models/users')
 
 const layout2 = '../views/layouts/main'
 
-router.get('', async (req, res) => {
-
+router.get('/login', async (req, res) => {
     const locals = {
         title: "NodeJS Blog",
         description: "Simple blog created with nodejs"
     }
 
     try {
-        const data = await Users.find()
-        res.render('index', { locals, data });
+        res.render('/login')
     } catch(error) {
         console.log(error)
     }
-
-
-
 })
 
 
 
+   
 
 
-router.get('/login', async (req, res) => {
 
-    
-    const locals = {
-        title: "NodeJS Blog",
-        description: "Simple blog created with nodejs"
-    }
-
-
-    res.render('login', { locals, layout: layout2 })
-})
 
 router.post('/login', async (req, res) => {
-
     try {
 
         const { username, password } = req.body
         console.log(req.body)
 
         res.redirect('/')
+        const user = await Users.findOne({ username })
+
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid Credentials'})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid Credentials'})
+        }
+
+        const token = jwt.sign({ userId: user._id}, jwtSecret)
+        res.cookie('token', token, {httpOnly: true})
+
+        res.send('Logged in')
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/register', async (req, res) => {
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Online News site created with nodeJS"
+    }
+
+    res.render('register', { locals, layout: layout2})
+})
+
+router.post('/register', async (req, res) => {
+    try {
+
+        const { username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        try {
+            const user = await Users.create({ username, password:hashedPassword})
+            res.status(201).json({ message: 'User Created', user})
+        } catch (error) {
+            console.log(error)
+        }
 
     } catch (error) {
         console.log(error)
