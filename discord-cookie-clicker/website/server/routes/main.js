05c949af70d996c151e02316fcd3a5774eda21ce
Glafyr <felixborrm@gmail.com>
2024-02-08 22:37:34 +0100

    Update main.js

--- discord-cookie-clicker/website/server/routes/main.js
+++ discord-cookie-clicker/website/server/routes/main.js
@@ -21,16 +21,12 @@ router.get('/redirect', async  (req, res) => {
             code: code,
             redirect_uri: 'http://localhost:5000/redirect/'
          })
-    
-         const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-         }
 
 
     
         const output = await axios.post('https://discord.com/api/oauth2/token', postData, {
             headers: {
-                headers
+                'Content-Type': 'application/x-www-form-urlencoded'
             }
          })
 
@@ -43,34 +39,32 @@ router.get('/redirect', async  (req, res) => {
                 },
             })
 
-            console.log(output.data, userinfo.data)
+            const postData1 = querystring.stringify({
+                client_id: process.env.CLIENTID,
+                client_secret: process.env.CLIENTSECRET,
+                grant_type: 'refresh_token',
+                redirect_uri: 'http://localhost:5000/redirect/',
+                refresh_token: output.data.refresh_token
+             })
+    
+    
+        
+            const refresh = await axios.post('https://discord.com/api/oauth2/token', postData1, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+             })
+
+             console.log(output.data, userinfo.data, refresh.data)
 
         } else {
             res.redirect('/')
         }
+
          
      } else {
         res.redirect('/')
      }
-    // const postData = querystring.stringify({
-    // client_id: process.env.CLIENTID,
-    // client_secret: process.env.CLIENTSECRET,
-    // grant_type: 'authorization_code',
-    // code: code,
-    // redirect_uri: 'http://localhost:5000/redirect/'
-    // });
-
-    // const headers = {
-    //     'Content-Type': 'application/x-www-form-urlencoded'
-    // };
-
-    // axios.post('https://discord.com/api/oauth2/token', postData, { headers: headers })
-    // .then(response => {
-    //         console.log(response.data)
-    // })
-    // .catch(error => {
-    //         console.log(error)
-    // });
     })
 
 
