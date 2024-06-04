4d6ae4f4128561f17031d41dad8988b3d96d362b
Glafyr <felixborrm@gmail.com>
2024-06-04 15:34:12 +0200

    ewr

--- discord-clicker/frontend/src/compontents/Navbar.tsx
+++ discord-clicker/frontend/src/compontents/Navbar.tsx
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import { FaUser } from "react-icons/fa";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+import { motion } from "framer-motion";
 
 const Navbar: React.FC = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  const [userOpen, setUserOpen] = useState(false);
   const [logedIn, setLogedIn] = useState<boolean>(false);
 
   const [user, setUser] = useState({
@@ -11,6 +15,15 @@ const Navbar: React.FC = (): JSX.Element => {
     avatar: "",
     id: "",
   });
+
+  const changeUserOpen = () => {
+    if (!logedIn) {
+      window.location.href =
+        "https://discord.com/oauth2/authorize?client_id=1243668312597074021&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fdiscord%2Foauth%2Fauthenticate%2F&scope=identify+connections+guilds.join+gdm.join+email+guilds+guilds.members.read";
+    } else {
+      setUserOpen(!userOpen);
+    }
+  };
 
   useEffect(() => {
     const token = Cookies.get("token");
@@ -38,40 +51,70 @@ const Navbar: React.FC = (): JSX.Element => {
 
   return (
     <div className="nav flex items-center justify-between p-4">
-      <a href="/">
-        <h1 className="logo text-white font-bold text-5xl">
+      <a href="/" className="z-20">
+        {/* <h1 className="logo text-white font-bold text-5xl">
           Clicker <span className="italic text-blue-500">Cord</span>
-        </h1>
+        </h1> */}
+        <img src="./Logo.png" alt="" className="sm:h-24 h-16 " />
       </a>
-      <div className="links flex h-full items-center gap-10">
-        <a
-          href="#"
-          className="text-2xl text-white hover:underline underline-offset-4"
+      <div className="flex h-full items-center gap-5 sm:gap-10">
+        <div className="links hidden md:flex h-full items-center gap-10">
+          <a
+            href="#"
+            className="text-2xl text-white hover:underline underline-offset-4"
+          >
+            Link1
+          </a>
+          <a
+            href="#"
+            className="text-2xl text-white hover:underline underline-offset-4"
+          >
+            Link2
+          </a>
+          <a
+            href="#"
+            className="text-2xl text-white hover:bg-blue-600 transition-all duration-300 ease-out w-32 rounded-lg flex items-center justify-center h-14 bg-blue-500"
+          >
+            Link3
+          </a>
+        </div>
+        <div className="md:hidden flex ">
+          {open ? (
+            <RiCloseFill
+              size={60}
+              className="z-20"
+              color="white"
+              onClick={() => setOpen(!open)}
+            />
+          ) : (
+            <RiMenu3Fill
+              size={50}
+              className="z-20"
+              color="white"
+              onClick={() => setOpen(!open)}
+            />
+          )}
+        </div>
+        <motion.div
+          variants={{
+            hidden: { width: 0, opacity: 0, pointerEvents: "none" },
+            show: { width: "100vw", opacity: 1, transition: { duration: 0.3 } },
+          }}
+          initial="hidden"
+          animate={open ? "show" : "hidden"}
+          exit="hidden"
+          className=" absolute top-0 left-0 h-screen bg-gray-950 z-0 flex flex-col items-center justify-center text-white"
         >
-          Link1
-        </a>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </motion.div>
         <a
-          href="#"
-          className="text-2xl text-white hover:underline underline-offset-4"
-        >
-          Link2
-        </a>
-        <a
-          href="#"
-          className="text-2xl text-white hover:bg-blue-600 transition-all duration-300 ease-out w-32 rounded-lg flex items-center justify-center h-14 bg-blue-500"
-        >
-          Link3
-        </a>
-        <a
-          href={
-            logedIn
-              ? "/dashboard"
-              : "https://discord.com/oauth2/authorize?client_id=1243668312597074021&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fdiscord%2Foauth%2Fauthenticate%2F&scope=identify+connections+guilds.join+gdm.join+email+guilds+guilds.members.read"
-          }
+          onClick={changeUserOpen}
           className={
             logedIn
-              ? "rounded-full overflow-hidden h-14 w-14 flex items-center justify-center"
-              : "user bg-blue-500 p-1 rounded-full overflow-hidden"
+              ? "rounded-full overflow-hidden h-14 w-14 flex items-center justify-center cursor-pointer z-20"
+              : "user bg-blue-500 p-1 rounded-full overflow-hidden cursor-pointer z-20"
           }
         >
           {logedIn ? (
@@ -83,6 +126,32 @@ const Navbar: React.FC = (): JSX.Element => {
             <FaUser size={45} color="white" className="relative top-1" />
           )}
         </a>
+        <motion.div
+          variants={{
+            hidden: { height: 0, opacity: 0, pointerEvents: "none" },
+            show: { height: "auto", opacity: 1, pointerEvents: "all" },
+          }}
+          initial="hidden"
+          animate={userOpen ? "show" : "hidden"}
+          exit="hidden"
+          className={
+            "flex flex-col absolute bg-gray-900 right-6 top-28 w-44 items-center rounded-xl z-40"
+          }
+        >
+          <h1 className="text-xl mt-4 font-bold text-white">User</h1>
+          <a href="#" className="text-center text-[#c8c8c8] font-extralight">
+            logged in as {user.username}
+          </a>
+          <a href="/profile" className=" text-white text-xl font-light mt-5">
+            Profile
+          </a>
+          <a
+            href="/dashboard"
+            className="mt-10 mb-10 text-white text-xl font-light"
+          >
+            Dashboard
+          </a>
+        </motion.div>
       </div>
     </div>
   );
